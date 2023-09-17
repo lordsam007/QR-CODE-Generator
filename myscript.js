513be9809565381ce33d8ca28d95277ca5fe003a
lordsam007 <samdevelops7@gmail.com>
2023-09-17 18:26:30 +0530

    QR code Generator Extension

---
+++ myscript.js
@@ -0,0 +1,9 @@
+﻿const elinput = document.getElementById("input");
+const elcreate = document.getElementById("create");
+const elimg = document.getElementById("img");
+
+const url = "https://chart.apis.google.com/chart?chs=300x300&cht=qr&chl=";
+
+elcreate.onclick = () => {
+  elimg.src = url + encodeURIComponent(elinput.value);
+}
